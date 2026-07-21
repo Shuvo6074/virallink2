f07d71845751ae086a40bdcfda687796e3dcff2e
Shuvo6074 <shuvoss504@gmail.com>
2026-07-21 15:49:53 +0600

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async rewrites() {
+    return [
+      { source: '/sitemap.xml', destination: '/api/sitemap' },
+    ];
+  },
+};
+
+module.exports = nextConfig;
