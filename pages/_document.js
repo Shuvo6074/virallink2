4198766b3db32e083c44116986aed5eda7592b81
Shuvo6074 <shuvoss504@gmail.com>
2026-07-23 01:22:00 +0600

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,4 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
@@ -8,11 +8,25 @@ export default function Document() {
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
+
+        <meta name="google-site-verification" content="XGdM1Gf3FOdKjE7clTPbSFMyv92T7VL8y9Ox5hqCMwE" />
+        <meta name="robots" content="index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1" />
+        <meta name="googlebot" content="index, follow" />
+        <meta name="rating" content="adult" />
+        <meta name="rating" content="RTA-5042-1996-1400-1577-RTA" />
+        <link rel="canonical" href="https://virallink2.site/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="BD Viral Hub" />
+        <meta property="og:locale" content="bn_BD" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=DM+Sans:wght@300;400;500;600&display=swap" rel="stylesheet" />
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
-    }
+  );
+}
