ebb25a594e8668f05d7fc4d2d64a858d8b945ce3
Shuvo6074 <shuvoss504@gmail.com>
2026-08-02 13:44:17 +0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
+import Script from 'next/script';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-BF6FEVSLHZ"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-BF6FEVSLHZ');
+        `}
+      </Script>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
