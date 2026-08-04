aa8b13b9e4d3abf99fb3a5893c06115b2d374682
Shuvo6074 <shuvoss504@gmail.com>
2026-08-04 15:35:50 +0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,18 @@
+import Head from 'next/head';
 import Script from 'next/script';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
+
       <Script
         strategy="afterInteractive"
         src="https://www.googletagmanager.com/gtag/js?id=G-BF6FEVSLHZ"
